43d6a5f2972c10c8a8c3ea242e7270ae76ed4249
Owusu Ansah Joy <owusujoyansah@gmail.com>
2025-07-12 00:41:54 +0100

    Added my query service

--- Blog/comments/index.js
+++ Blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express()
 
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req,res) => {
+app.post("/posts/:id/comments",async  (req,res) => {
   const commentId = randomBytes(4).toString('hex')
   const {content} = req.body
 
@@ -23,8 +24,23 @@ app.post("/posts/:id/comments", (req,res) => {
   comments.push({id: commentId, content: content});
   commentsByPostId[req.params.id] = comments;
 
+  // Making a post request to the event-bus server
+  await axios.post('http://localhost:4005/events', {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
+
   res.status(201).send(comments)
 });
+
+app.post('/events', (req, res) => {
+  console.log('Recieved Event: ', req.body.type);
+  res.send({});
+})
 
 app.listen(4001, () => {
 console.log("LISTENING ON 4001")
--- Blog/event-bus/index.js
+++ Blog/event-bus/index.js
@@ -8,17 +8,17 @@ app.use(bodyParser.json());
 
 
 app.post('/events', (req,res) => {
-  const event = req.body;
+  const event = req.body; 
 
   // I am assuming all these requests are going to be successful,
   //  and that explains why I dont have any serious implementation
   axios.post('http://localhost:4000/events', event ); // for posts
   axios.post('http://localhost:4001/events', event ); // for comments
-  axios.post('http://localhost:4002/events', event ); // for query-service
+  // axios.post('http://localhost:4002/events', event ); // for query-service
 
   res.send({status : 'OK'});
 })
 
-app.listen(40005, () => {
+app.listen(4005, () => {
   console.log('Listening on 4005')
 })
